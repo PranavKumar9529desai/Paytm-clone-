b544428e2cab74f003e4de00f4310822027d08f0
PranavKumar9529desai <dpranav7745@gmail.com>
2024-02-07 13:32:06 +0530

    complete backend and frontend

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,17 @@
-const express = require("express");
+const db = require('./db/db');
+const express = require('express') ;
+const rootRouter  = require('./routes/index') ;
+const app =  express() ;
+const cors = require('cors');
+const bodyparser =  require('body-parser');
 
 
+app.use(cors()) ;
+app.use(express.json());
+
+app.use("/api/v1" ,rootRouter) ;
+
+
+app.listen(process.env.PORT || 3000,()=>{
+  console.log("Server is running on port 3000");
+})
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-
+import {BrowserRouter , Routes , Route ,useNavigate } from 'react-router-dom';
+import {Dashboard} from './Routes/Dashboard';
+import {SignUp} from './Routes/SignUp';
+import {SignIn} from './Routes/SignIn';
+import {Send} from './Routes/Send';
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
-  )
-}
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/dashboard'   element={<Dashboard/>} />
+        <Route path='/signin'    element={<SignIn/>} />
+        <Route path='/signup'   element={<SignUp/>} />
+        <Route path='/send'   element={<Send/>} />
+      </Routes>
+    </BrowserRouter>
+</>)}
 
 export default App
+
+
